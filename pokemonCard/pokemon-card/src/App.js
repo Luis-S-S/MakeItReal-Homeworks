0d4a2e86226a18a0e041b62787ad71e5729d3271
Luis Salcedo Salas <rideluis@gmail.com>
2022-03-09 16:17:38 -0500

    feat: React exercises added in pokemon card

--- pokemonCard/pokemon-card/src/App.js
+++ pokemonCard/pokemon-card/src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
 import PokemonCard from './components/PokemonCard.jsx';
 import pokemonList from './assets/pokemons.json';
+import OnionHater from './components/OnionHater';
+import Destiny from './components/Destiny'
 
 function App() {
   return (
     <div className="App">
-      {pokemonList.map((pokemon, idx) => 
-
-        <PokemonCard key={idx} pokemon={pokemon}/>
-
-      )}
+      {pokemonList.map((pokemon, idx) => <PokemonCard key={idx} pokemon={pokemon}/>)}
+      <OnionHater/>
+      <Destiny/>
     </div>
   );
 }
